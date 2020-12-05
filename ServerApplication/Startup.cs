939b4072ea404431c88e09838fa58bcd0d966fe4
Sadra <sadra.z.kh@gmail.com>
2020-12-05 18:59:01 +0330

    web framework

--- ServerApplication/Startup.cs
+++ ServerApplication/Startup.cs
@@ -11,22 +11,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common;
+using WebFramework.Configuration;
+using WebFramework.CustomMapping;
+using WebFramework.Middlewares;
+using WebFramework.Swagger;
 
 namespace ServerApplication
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        private readonly SiteSettings _siteSetting;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _siteSetting = configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>();
         }
 
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.Configure<SiteSettings>(Configuration.GetSection(nameof(SiteSettings)));
+            services.InitializeAutoMapper();
+            services.AddDbContext(Configuration);
+            services.AddCustomIdentity(_siteSetting.IdentitySettings);
+            services.AddMinimalMvc();
+            services.AddElmahCore(Configuration, _siteSetting);
+            services.AddJwtAuthentication(_siteSetting.JwtSettings);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -44,16 +59,36 @@ namespace ServerApplication
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ServerApplication v1"));
             }
 
+            app.IntializeDatabase();
+
+            app.UseCustomExceptionHandler();
+
+            app.UseHsts(env);
+
             app.UseHttpsRedirection();
+
+            app.UseElmahCore(_siteSetting);
+
+            //app.UseSwaggerAndUI();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            //Use this config just in Develoment (not in Production)
+            //app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseEndpoints(config =>
             {
-                endpoints.MapControllers();
+                config.MapControllers(); // Map attribute routing
+                //    .RequireAuthorization(); Apply AuthorizeFilter as global filter to all endpoints
+                //config.MapDefaultControllerRoute(); // Map default route {controller=Home}/{action=Index}/{id?}
             });
+
+            //Using 'UseMvc' to configure MVC is not supported while using Endpoint Routing.
+            //To continue using 'UseMvc', please set 'MvcOptions.EnableEndpointRouting = false' inside 'ConfigureServices'.
+            //app.UseMvc();
         }
     }
 }
